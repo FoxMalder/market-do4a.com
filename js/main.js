cbd5e012aefa282a5d3e2ee970369623db7c6eb0
Мещеряков Роман <staremang@ya.ru>
2019-03-05 03:32:39 +0700

    0.9.9

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 $(document).ready(function () {
+
+    var $starSlider = $('.stars__slider');
+    var starSliderInited = false;
+
+    function sliderInit() {
+        if ( $(window).width() < 768 && !starSliderInited ) {
+            starSliderInited = true;
+            $starSlider.addClass('owl-carousel');
+            $starSlider.owlCarousel({
+                loop: true,
+                items: 1,
+                nav: false,
+                dots: false,
+                margin: 10,
+                autoWidth: true,
+                autoHeight: true,
+                center: true,
+            });
+        } else if ( $(window).width() >= 768 && starSliderInited ) {
+            $starSlider.trigger('destroy.owl.carousel');
+            $starSlider.removeClass('owl-carousel');
+            starSliderInited = false;
+        }
+    }
+
+    sliderInit();
+    $(window).resize(sliderInit);
+
     $('.best-articles__slider').owlCarousel({
         loop: false,
         items: 1,
         nav: false,
         dots: false,
+        // center: true,
         margin: 10,
+        autoWidth: true,
         responsive: {
             768: {
-                autoWidth: true,
+                center: false,
                 margin: 24,
             },
             1240: {
@@ -25,6 +55,19 @@ $(document).ready(function () {
         nav: false,
         dots: false,
         margin: 10,
+        navContainerClass: 'slider-nav',
+        navText: [
+            '<svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.5 5.96968L7.5 5.96968L7.5 9.5498L0 5.21968L7.5 0.88955L7.5 4.46968L16.5 4.46968L16.5 5.96968Z"/>\n' +
+            '</svg>',
+            '<svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M0.5 3.96978L9.5 3.96978L9.5 0.389649L17 4.71978L9.5 9.0499L9.5 5.46978L0.5 5.46978L0.5 3.96978Z"/>\n' +
+            '</svg>',
+        ],
+        navClass: [
+            'btn slider-nav__button slider-nav__button_left',
+            'btn slider-nav__button slider-nav__button_right',
+        ],
         responsive: {
             768: {
                 autoWidth: false,
