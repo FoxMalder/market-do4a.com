db12cec8fe07a8ea2d025974f26f9b76e18cbd87
Мещеряков Роман <staremang@ya.ru>
2019-04-10 14:52:52 +0700

    Правки 10.04 (№8)

--- src/js/modules/Input.js
+++ src/js/modules/Input.js
@@ -1,11 +1,17 @@
 export default function Input() {
   const $input = $('.input-field__input');
 
-  $input.on('focus', function () {
+  $input.each(function() {
+    if ($(this).val() !== '') {
+      $(this).parent().addClass('active');
+    }
+  });
+
+  $input.on('focus', function() {
     $(this).parent().addClass('active');
   });
 
-  $input.on('blur', function () {
+  $input.on('blur', function() {
     if ($(this).val() === '') {
       $(this).parent().removeClass('active');
     }
