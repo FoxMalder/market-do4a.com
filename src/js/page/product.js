7f4ffd41f79fcb47ebcf3c539d165947758ba855
Мещеряков Роман <staremang@ya.ru>
2019-08-21 23:17:08 +0700

    Продукт / Hotfix 4

--- src/js/page/product.js
+++ src/js/page/product.js
@@ -22,7 +22,7 @@ import ProductReviews from '../components/product/Reviews.vue';
 Vue.filter('formatNumber', (value) => {
   // if (!value) return '';
   // return value.toLocaleString();
-  return '@' + value.toString();
+  return `@${value.toString()}`;
 });
 
 Vue.filter('formatPrice', (value) => {
@@ -30,12 +30,6 @@ Vue.filter('formatPrice', (value) => {
   // return value.toLocaleString();
   return `${value.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')} ₽`;
 });
-//
-// Vue.filter('formatUnit', (value) => {
-//   // if (!value) return '';
-//   // return value.toLocaleString();
-//   return `${value.toString()} ₽`;
-// });
 
 
 /**
@@ -154,13 +148,42 @@ class ProductPage {
     };
 
 
+    this.elements = {
+      detailReviewsCount: document.querySelector('.p-detail__reviews'),
+      reviewsCount: document.querySelector('.p-reviews-header__count'),
+      availability: document.querySelector('.p-section-availability'),
+      breadcumpsThisPage: document.querySelector('.breadcumps__page'),
+    };
+
+    // this.$elements = {
+    //   detailReviewsCount: $('.p-detail__reviews'),
+    //   reviewsCount: $('.p-reviews-header__count'),
+    //   availability: $('.p-section-availability'),
+    //   breadcumpsThisPage: $('.breadcumps__page'),
+    // };
+
+
     store.dispatch('product/getAllPacking');
 
     $('.p-review-form').on('submit', this.addReview);
     $('.p-modal-form').on('submit', this.addReview);
   }
 
-  initVue() {
+  initVue(subscribe = false) {
+    if (subscribe) {
+      store.subscribe((mutation, state) => {
+        // console.log(mutation.type);
+        // console.log(mutation.payload);
+
+        if (mutation.type === 'product/SET_ACTIVE_OFFER_ID') {
+          this.onChangeOffer(state, mutation.payload);
+        }
+        if (mutation.type === 'product/SET_ACTIVE_PACKING_ID') {
+          this.onChangePacking(state, mutation.payload);
+        }
+      });
+    }
+
     new Vue({
       store,
       render: h => h(ProductDetail),
@@ -187,55 +210,69 @@ class ProductPage {
       store,
       render: h => h(StarRating),
     }).$mount('.p-review-form__stars');
+
     new Vue({
       store,
       render: h => h(StarRating),
     }).$mount('.p-modal-form__rating-stars');
   };
 
-  initStoreListeners() {
-    store.subscribe((mutation, state) => {
-      // console.log(mutation.type);
-      // console.log(mutation.payload);
+  /**
+   * При изменении фасовки
+   *
+   * @param state - новое состояние хранилища
+   * @param payload - переданные данные
+   */
+  onChangePacking = (state, payload) => {
+    const activePacking = payload;
 
-      if (mutation.type === 'product/SET_ACTIVE_OFFER_ID') {
-        if (mutation.payload.count > 0) {
-          $('.p-section-availability').show();
+    document.title = activePacking.title;
+    window.history.replaceState(null, null, activePacking.url);
 
-          [].forEach.call(document.querySelectorAll('[data-store-id]'), (item) => {
-            const { storeId } = item.dataset;
-            const stockEl = item.querySelector('.p-section-availability__col_stock > .p-section-availability__td');
-            const pickupEl = item.querySelector('.p-section-availability__col_pickup > .p-section-availability__td');
-            const pickupNoteEl = item.querySelector('.p-section-availability__col_pickup > .p-section-availability__note');
+    if (this.elements.breadcumpsThisPage) this.elements.breadcumpsThisPage.innerHTML = activePacking.name;
+    if (this.elements.detailReviewsCount) this.elements.detailReviewsCount.innerHTML = `${activePacking.review} ${Utils.declOfNum(activePacking.review, ['отзыв', 'отзыва', 'отзывов'])}`;
+    if (this.elements.reviewsCount) this.elements.reviewsCount.innerHTML = `${activePacking.review > 0 ? activePacking.review : ''}`;
+  };
+
+  /**
+   * При изменении оффера
+   *
+   * @param state - новое состояние хранилища
+   * @param payload - переданные данные
+   */
+  onChangeOffer = (state, payload) => {
+    const activeOffer = payload;
+
+    if (activeOffer.count > 0) {
+      if (this.elements.availability) this.elements.availability.style.display = '';
+
+      [].forEach.call(document.querySelectorAll('[data-store-id]'), (item) => {
+        const { storeId } = item.dataset;
+        const stockEl = item.querySelector('.p-section-availability__col_stock > .p-section-availability__td');
+        const pickupEl = item.querySelector('.p-section-availability__col_pickup > .p-section-availability__td');
+        const pickupNoteEl = item.querySelector('.p-section-availability__col_pickup > .p-section-availability__note');
 
 
-            if ({}.hasOwnProperty.call(mutation.payload.available_store, storeId)) {
-              const count = Math.min(Math.ceil(mutation.payload.available_store[storeId] / 20), 5);
+        if ({}.hasOwnProperty.call(activeOffer.available_store, storeId)) {
+          const count = Math.min(Math.ceil(activeOffer.available_store[storeId] / 20), 5);
 
-              if (stockEl) {
-                stockEl.innerHTML = global.product.textAvailable[count] + this.rectRatingHTML[count];
-              }
+          if (stockEl) {
+            stockEl.innerHTML = global.product.textAvailable[count] + this.rectRatingHTML[count];
+          }
 
-              if (pickupEl) {
-                pickupEl.innerHTML = count > 0 ? 'Через час' : 'Недоступен';
-              }
+          if (pickupEl) {
+            pickupEl.innerHTML = count > 0 ? 'Через час' : 'Недоступен';
+          }
 
-              if (pickupNoteEl) {
-                pickupNoteEl.innerHTML = count > 0 ? 'Требуется предварительный заказ на сайте' : '';
-              }
-            }
-          });
-        } else {
-          $('.p-section-availability').hide();
+          if (pickupNoteEl) {
+            pickupNoteEl.innerHTML = count > 0 ? 'Требуется предварительный заказ на сайте' : '';
+          }
         }
-      }
-
-      if (mutation.type === 'product/SET_ACTIVE_PACKING_ID') {
-        document.querySelector('.p-detail__reviews').innerHTML = `${mutation.payload.review} ${Utils.declOfNum(mutation.payload.review, ['отзыв', 'отзыва', 'отзывов'])}`;
-        document.querySelector('.p-reviews-header__count').innerHTML = `${mutation.payload.review > 0 ? mutation.payload.review : ''}`;
-      }
-    });
-  }
+      });
+    } else {
+      if (this.elements.availability) this.elements.availability.style.display = 'none';
+    }
+  };
 
 
   /**
@@ -263,13 +300,12 @@ class ProductPage {
 
 $(() => {
   $('[data-toggle="tooltip"]').tooltip();
-  $('.p-control-select__header').dropdown({
-    display: 'static',
-  });
+  $('.p-control-select__header').dropdown({ display: 'static' });
 
 
   // Фиксация блока с фото на планшете
-  if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1240) {
+  if (document.documentElement.clientWidth >= 768
+    && document.documentElement.clientWidth < 1240) {
     new Sticky('.p-detail__left-block', {
       marginTop: 90,
       // stickyClass: 'is-sticky',
@@ -283,9 +319,6 @@ $(() => {
   });
 
 
-  // initProductSelect();
-  // initProductCounter();
-  // initCartButton();
   initStoreInfo();
   initCollapse();
 
