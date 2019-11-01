58c4419802ef4da90189a3b76a0f9e4a335c8e26
Мещеряков Роман <staremang@ya.ru>
2019-11-02 00:27:38 +0700

    Уменьшен размер, исправлена заглушка корзины

--- webpack.config.js
+++ webpack.config.js
@@ -360,7 +360,7 @@ const devConfig = {
 
 const prodConfig = {
   mode: 'production',
-  devtool: 'eval-source-map',
+  // devtool: 'eval-source-map',
   // output: {
   //   publicPath: '/static/dist/',
   // },
