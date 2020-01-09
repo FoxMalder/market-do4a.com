06752534721e7df56a3b3cea9c463ed0b0f4e894
Мещеряков Роман <staremang@ya.ru>
2020-01-10 01:15:43 +0700

    Public Path

--- webpack.config.js
+++ webpack.config.js
@@ -439,9 +439,9 @@ const devConfig = {
 const prodConfig = {
   mode: 'production',
   // devtool: 'eval-source-map',
-  // output: {
-  //   publicPath: '/static/dist/',
-  // },
+  output: {
+    publicPath: '/static/dist/',
+  },
   module: {
     rules: [
       // {
@@ -559,7 +559,7 @@ const prodConfig = {
   plugins: [
     new CleanWebpackPlugin(),
 
-    new webpack.BannerPlugin('Your copyright notice'),
+    new webpack.BannerPlugin('Front: Roman Meshcheryakov'),
 
     new CssUrlRelativePlugin(),
     new ExtractCssChunksPlugin({
