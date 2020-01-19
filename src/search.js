3a22cb584a03a35bd6c37576bbec5e23da382866
Мещеряков Роман <staremang@ya.ru>
2020-01-19 16:17:21 +0700

    [Search Page] Beta

--- src/search.js
+++ src/search.js
@@ -1,37 +1,52 @@
 import ready from 'domready';
 
-// import '@/App';
-// import '@/common';
 // import './simple';
-// import Swiper from 'swiper';
-import { Navigation, A11y, Swiper } from 'swiper/js/swiper.esm';
-Swiper.use([Navigation, A11y]);
+import { Navigation, A11y, Swiper, Mousewheel } from 'swiper/js/swiper.esm';
+
 
 ready(() => {
-  new Swiper('.search-page-slider', {
-    touchEventsTarget: 'wrapper',
+  if (document.documentElement.clientWidth >= 1240) {
+    Swiper.use([Navigation, A11y, Mousewheel]);
 
-    containerModifierClass: 'slider_',
-    slideClass: 'slider__slide',
-    slideBlankClass: 'slider__slide_invisible-blank',
-    slideActiveClass: 'slider__slide_active',
-    slideDuplicateActiveClass: 'slider__slide_duplicate-active',
-    slideVisibleClass: 'slider__slide_visible',
-    slideDuplicateClass: 'slider__slide_duplicate',
-    slideNextClass: 'slider__slide_next',
-    slideDuplicateNextClass: 'slider__slide_duplicate-next',
-    slidePrevClass: 'slider__slide_prev',
-    slideDuplicatePrevClass: 'slider__slide_duplicate-prev',
-    wrapperClass: 'slider__wrapper',
+    new Swiper('.search-page-slider', {
+      touchEventsTarget: 'wrapper',
 
-    slidesPerView: 'auto',
-    // spaceBetween: 10,
+      wrapperClass: 'slider__wrapper',
+      containerModifierClass: 'slider_',
+      slideClass: 'slider__slide',
+      slideBlankClass: 'slider__slide_invisible-blank',
+      slideActiveClass: 'slider__slide_active',
+      slideDuplicateActiveClass: 'slider__slide_duplicate-active',
+      slideVisibleClass: 'slider__slide_visible',
+      slideDuplicateClass: 'slider__slide_duplicate',
+      slideNextClass: 'slider__slide_next',
+      slideDuplicateNextClass: 'slider__slide_duplicate-next',
+      slidePrevClass: 'slider__slide_prev',
+      slideDuplicatePrevClass: 'slider__slide_duplicate-prev',
 
-    navigation: {
-      nextEl: '.slider-button_next',
-      prevEl: '.slider-button_prev',
-      disabledClass: 'slider-button_disabled',
-      hiddenClass: 'slider-button_hidden',
-    },
-  });
+      slidesPerView: 'auto',
+      watchSlidesVisibility: true,
+
+      freeMode: true,
+      freeModeSticky: true,
+      freeModeMomentumRatio: 0.4,
+      freeModeMomentumVelocityRatio: 0.5,
+      mousewheel: {
+        // enabled: true,
+        forceToAxis: true,
+        invert: true,
+        // releaseOnEdges: false,
+      },
+      navigation: {
+        nextEl: '.slider-button_next',
+        prevEl: '.slider-button_prev',
+        disabledClass: 'slider-button_disabled',
+        hiddenClass: 'slider-button_hidden',
+      },
+      a11y: {
+        prevSlideMessage: 'Предыдущий слайд',
+        nextSlideMessage: 'Следующий слайд',
+      },
+    });
+  }
 });
