18ad361dbe462a3e4dccf4f8412a72d2ee016f6a
Мещеряков Роман <staremang@ya.ru>
2020-02-24 18:54:42 +0700

    Build v1.4.0

--- dist/js/simple.js
+++ dist/js/simple.js
@@ -1,5 +1,5 @@
 /*!
- * Front for marketdo4a.com 1.4.0-beta.2 
+ * Front for marketdo4a.com 1.4.0 
  * By Roman Meshcheryakov 2020
  */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{14:function(module,exports){eval("module.exports = jQuery;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vZXh0ZXJuYWwgXCJqUXVlcnlcIj9jZDBjIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6IjE0LmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSBqUXVlcnk7Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///14\n")}},[[47,1,0]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{14:function(n,o){n.exports=jQuery}},[[47,1,0]]]);
